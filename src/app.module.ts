9e5e4246784a17b05e66dcbdde1164a160cfff02
wam <dorothy_007@naver.com>
2024-04-28 17:29:13 +0900

    3.6 Validating ConfigService

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,6 +4,7 @@ import { ConfigModule } from "@nestjs/config";
 import { GraphQLModule } from "@nestjs/graphql";
 import { TypeOrmModule } from "@nestjs/typeorm";
 import { RestaurantsModule } from "./restaurants/restaurants.module";
+import * as Joi from "joi";
 
 @Module({
   imports: [
@@ -11,6 +12,14 @@ import { RestaurantsModule } from "./restaurants/restaurants.module";
       isGlobal: true, // ConfigModule이 전역으로 설정되어야 하는지 여부
       envFilePath: process.env.NODE_ENV === "dev" ? ".env.dev" : ".dev.test",
       ignoreEnvFile: process.env.NODE_ENV === "prod", // 서버에 deploy 할 때 환경변수 파일을 사용하지 않기
+      validationSchema: Joi.object({
+        NODE_ENV: Joi.string().valid("dev", "prod").required(),
+        DB_HOST: Joi.string().required(),
+        DB_PORT: Joi.string().required(),
+        DB_USERNAME: Joi.string().required(),
+        DB_PASSWORD: Joi.string().required(),
+        DB_DATABASE: Joi.string().required(),
+      }),
     }),
     TypeOrmModule.forRoot({
       type: "postgres",
