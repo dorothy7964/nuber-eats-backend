64ea371786130b34596be36d0991af210e1fc742
wam <dorothy_007@naver.com>
2024-04-28 14:37:53 +0900

    3.3 TypeORM Setup

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,14 +2,25 @@ import { ApolloDriver, ApolloDriverConfig } from "@nestjs/apollo";
 import { Module } from "@nestjs/common";
 import { GraphQLModule } from "@nestjs/graphql";
 import { RestaurantsModule } from "./restaurants/restaurants.module";
+import { TypeOrmModule } from "@nestjs/typeorm";
 
 @Module({
   imports: [
+    RestaurantsModule,
+    TypeOrmModule.forRoot({
+      type: "postgres",
+      host: "localhost",
+      port: 5432,
+      username: "wooami",
+      password: "0000", // localhost로 연결하고 있다면 패스워드를 작성하지 않거나 비밀번호가 틀려도 PostgreSQL은 그냥 연결 시켜준다.
+      database: "nuber-eats",
+      synchronize: true,
+      logging: true,
+    }),
     GraphQLModule.forRoot<ApolloDriverConfig>({
       driver: ApolloDriver,
       autoSchemaFile: true,
     }),
-    RestaurantsModule,
   ],
 })
 export class AppModule {}
