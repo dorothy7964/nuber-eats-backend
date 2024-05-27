b373b19fb04fce999c795cf620ea93eccd55ed06
wam <dorothy_007@naver.com>
2024-05-27 18:53:41 +0900

    5.5 Create Account Mutation part Three

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,9 +1,8 @@
 import { Injectable } from "@nestjs/common";
 import { InjectRepository } from "@nestjs/typeorm";
 import { Repository } from "typeorm";
-import { User } from "./entities/user.entity";
 import { CreateAccountInput } from "./dtos/create-account.dto";
-import { exist } from "joi";
+import { User } from "./entities/user.entity";
 
 @Injectable()
 export class UsersService {
@@ -11,17 +10,18 @@ export class UsersService {
     @InjectRepository(User) private readonly users: Repository<User>,
   ) {}
 
-  async createAccount({ email, password, role }: CreateAccountInput) {
+  async createAccount({
+    email,
+    password,
+    role,
+  }: CreateAccountInput): Promise<string | undefined> {
     try {
       const exists = await this.users.findOne({ where: { email } });
-
       if (exists) {
         return "There is a user with that email already";
       }
-
       /* 작성한 이메일이 존재하지 않는다면 작성한 계정 저장하기 */
       await this.users.save(this.users.create({ email, password, role }));
-      return true;
     } catch (e) {
       return "Couldn`t create account";
     }
