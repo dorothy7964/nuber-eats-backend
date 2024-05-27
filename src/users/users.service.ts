c9dd600dff5a9110a6d06550371e64542236e43f
wam <dorothy_007@naver.com>
2024-05-27 22:52:45 +0900

    5.6 An Alternative Error

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -14,16 +14,17 @@ export class UsersService {
     email,
     password,
     role,
-  }: CreateAccountInput): Promise<string | undefined> {
+  }: CreateAccountInput): Promise<{ ok: boolean; error?: string }> {
     try {
       const exists = await this.users.findOne({ where: { email } });
       if (exists) {
-        return "There is a user with that email already";
+        return { ok: false, error: "There is a user with that email already" };
       }
       /* 작성한 이메일이 존재하지 않는다면 작성한 계정 저장하기 */
       await this.users.save(this.users.create({ email, password, role }));
+      return { ok: true };
     } catch (e) {
-      return "Couldn`t create account";
+      return { ok: false, error: "Couldn`t create account" };
     }
   }
 }
