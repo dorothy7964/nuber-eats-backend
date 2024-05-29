0c5f53e9fc1fce5249a70f55d672179478288f90
wam <dorothy_007@naver.com>
2024-05-29 21:47:46 +0900

    6.1 Generating JWT

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,14 +1,17 @@
 import { Injectable } from "@nestjs/common";
+import { ConfigService } from "@nestjs/config";
 import { InjectRepository } from "@nestjs/typeorm";
+import * as jwt from "jsonwebtoken";
 import { Repository } from "typeorm";
 import { CreateAccountInput } from "./dtos/create-account.dto";
-import { User } from "./entities/user.entity";
 import { LoginInput } from "./dtos/login.dto";
+import { User } from "./entities/user.entity";
 
 @Injectable()
 export class UsersService {
   constructor(
     @InjectRepository(User) private readonly users: Repository<User>,
+    private readonly config: ConfigService,
   ) {}
 
   async createAccount({
@@ -49,6 +52,7 @@ export class UsersService {
           error: "Wrong password",
         };
       }
+      const token = jwt.sign({ id: user.id }, this.config.get("SECRET_KEY"));
       return {
         ok: true,
         token: "a temporary token",
