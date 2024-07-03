752365498252be227263fd2e9bd8bab933e1e947
wam <dorothy_007@naver.com>
2024-07-04 03:13:42 +0900

    8.2 Mocking part Two

--- src/users/user.service.spec.ts
+++ src/users/user.service.spec.ts
@@ -5,6 +5,7 @@ import { User } from "./entities/user.entity";
 import { Verification } from "./entities/verification.entity";
 import { JwtService } from "src/jwt/jwt.service";
 import { MailService } from "src/mail/mail.service";
+import { Repository } from "typeorm";
 
 const mockRepository = {
   create: jest.fn(),
@@ -21,8 +22,13 @@ const mockMailService = {
   sendEmailDemo: jest.fn(),
 };
 
+type MockRepository<T = any> = Partial<
+  Record<keyof Repository<User>, jest.Mock>
+>;
+
 describe("UserService", () => {
   let service: UserService;
+  let userRepository: MockRepository<User>;
 
   beforeAll(async () => {
     const module = await Test.createTestingModule({
@@ -51,9 +57,14 @@ describe("UserService", () => {
       ],
     }).compile();
     service = module.get<UserService>(UserService);
+    userRepository = module.get(getRepositoryToken(User));
   });
 
   it("should be defined", () => {
+    expect(service).toBeDefined();
+  });
+
+  it("should fail if user exists", () => {
     expect(service).toBeDefined();
   });
 
