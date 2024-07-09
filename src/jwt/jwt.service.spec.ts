7a56a1754d9c8cb945a13408d19fc3beaebbf71a
wam <dorothy_007@naver.com>
2024-07-10 03:16:27 +0900

    9.1 JWT Sign Test

--- src/jwt/jwt.service.spec.ts
+++ src/jwt/jwt.service.spec.ts
@@ -1,8 +1,18 @@
 import { Test } from "@nestjs/testing";
-import { JwtService } from "./jwt.service";
+import * as jwt from "jsonwebtoken";
 import { CONFIG_OPTIONS } from "src/common/common.constants";
+import { JwtService } from "./jwt.service";
+import { Token } from "graphql";
 
 const TEST_KEY = "testKey";
+const USER_ID = 1;
+
+// 인자로는 npm module의 이름이 들어간다.
+jest.mock("jsonwebtoken", () => {
+  return {
+    sign: jest.fn(() => "TOKEN"),
+  };
+});
 
 describe("JwtService", () => {
   let service: JwtService;
@@ -25,6 +35,12 @@ describe("JwtService", () => {
     expect(service).toBeDefined();
   });
 
-  it.todo("sign");
-  it.todo("verify");
+  describe("sign", () => {
+    it("should return a signed token", () => {
+      const token = service.sign(USER_ID);
+      expect(typeof token).toBe("string");
+      expect(jwt.sign).toHaveBeenCalledTimes(1);
+      expect(jwt.sign).toHaveBeenCalledWith({ id: USER_ID }, TEST_KEY);
+    });
+  });
 });
