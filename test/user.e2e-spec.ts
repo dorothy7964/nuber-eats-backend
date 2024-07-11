b7eb27dd318e258757f9420fa9267b85a697d572
wam <dorothy_007@naver.com>
2024-07-12 02:25:49 +0900

    10.1 Setup part Two

--- test/user.e2e-spec.ts
+++ test/user.e2e-spec.ts
@@ -1,10 +1,11 @@
-import { Test, TestingModule } from "@nestjs/testing";
 import { INestApplication } from "@nestjs/common";
-import * as request from "supertest";
+import { Test, TestingModule } from "@nestjs/testing";
+import { DataSource, getConnection } from "typeorm";
 import { AppModule } from "../src/app.module";
 
 describe("UserModule (e2e)", () => {
   let app: INestApplication;
+  let dataSource: DataSource;
 
   beforeAll(async () => {
     const moduleFixture: TestingModule = await Test.createTestingModule({
@@ -14,12 +15,21 @@ describe("UserModule (e2e)", () => {
 
     app = moduleFixture.createNestApplication();
     await app.init();
+
+    dataSource = app.get(DataSource); // AppModule에서 DataSource 인스턴스를 가져오기
+    console.log("📢 [user.e2e-spec.ts:20]", dataSource);
   });
 
-  it.todo("me");
-  it.todo("userProfile");
-  it.todo("editProfile");
+  afterAll(async () => {
+    await dataSource.dropDatabase(); // 데이터베이스 삭제
+    await dataSource.destroy(); // 연결 해제
+    await app.close();
+  });
+
   it.todo("createAccount");
+  it.todo("userProfile");
   it.todo("login");
+  it.todo("me");
   it.todo("verifyEmail");
+  it.todo("editProfile");
 });
