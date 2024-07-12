751d56063eb81931ffe556059d978638b485b3d2
wam <dorothy_007@naver.com>
2024-07-12 19:48:09 +0900

    10.4 Testing login

--- test/user.e2e-spec.ts
+++ test/user.e2e-spec.ts
@@ -22,6 +22,7 @@ const testUser = {
 describe("UserModule (e2e)", () => {
   let app: INestApplication;
   let dataSource: DataSource;
+  let jwtToken: string;
 
   const baseTest = () => request(app.getHttpServer()).post(GRAPHQL_ENDPOINT);
   const publicTest = (query: string) => baseTest().send({ query });
@@ -59,8 +60,13 @@ describe("UserModule (e2e)", () => {
           `)
         .expect(200)
         .expect((res) => {
-          expect(res.body.data.createAccount.ok).toBe(true);
-          expect(res.body.data.createAccount.error).toBe(null);
+          const {
+            body: {
+              data: { createAccount },
+            },
+          } = res;
+          expect(createAccount.ok).toBe(true);
+          expect(createAccount.error).toBe(null);
         });
     });
 
@@ -92,8 +98,62 @@ describe("UserModule (e2e)", () => {
     });
   });
 
+  describe("login", () => {
+    it("should login with correct credentials", () => {
+      return publicTest(`
+          mutation {
+            login(input:{
+              email:"${testUser.email}",
+              password:"${testUser.password}",
+            }) {
+              ok
+              error
+              token
+            }
+          }
+        `)
+        .expect(200)
+        .expect((res) => {
+          const {
+            body: {
+              data: { login },
+            },
+          } = res;
+          expect(login.ok).toBe(true);
+          expect(login.error).toBe(null);
+          expect(login.token).toEqual(expect.any(String));
+          jwtToken = login.token;
+        });
+    });
+
+    it("should not be able to login with wrong credentials", () => {
+      return publicTest(`
+          mutation {
+            login(input:{
+              email:"${testUser.email}",
+              password:"xxx",
+            }) {
+              ok
+              error
+              token
+            }
+          }
+        `)
+        .expect(200)
+        .expect((res) => {
+          const {
+            body: {
+              data: { login },
+            },
+          } = res;
+          expect(login.ok).toBe(false);
+          expect(login.error).toBe("Wrong password");
+          expect(login.token).toBe(null);
+        });
+    });
+  });
+
   it.todo("userProfile");
-  it.todo("login");
   it.todo("me");
   it.todo("verifyEmail");
   it.todo("editProfile");
