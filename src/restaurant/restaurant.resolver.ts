e1d628e171bd9346587e3951a75978731cd4a9ad
wam <dorothy_007@naver.com>
2024-07-22 21:28:04 +0900

    11.17 Restaurant and Search

--- src/restaurant/restaurant.resolver.ts
+++ src/restaurant/restaurant.resolver.ts
@@ -28,6 +28,11 @@ import { Restaurant } from "./entities/restaurant.entity";
 import { RestaurantService } from "./restaurant.service";
 import { CategoryInput, CategoryOutput } from "./dtos/category.dto";
 import { RestaurantsInput, RestaurantsOutput } from "./dtos/restaurants.dto";
+import { RestaurantInput, RestaurantOutput } from "./dtos/restaurant.dto";
+import {
+  SearchRestaurantInput,
+  SearchRestaurantOutput,
+} from "./dtos/search-restaurant.dto";
 
 @Resolver(() => Restaurant)
 export class RestaurantResolver {
@@ -71,6 +76,20 @@ export class RestaurantResolver {
     @Args("input") restaurantsInput: RestaurantsInput,
   ): Promise<RestaurantsOutput> {
     return this.restaurantService.allRestaurants(restaurantsInput);
+  }
+
+  @Query(() => RestaurantOutput)
+  restaurant(
+    @Args("input") restaurantInput: RestaurantInput,
+  ): Promise<RestaurantOutput> {
+    return this.restaurantService.findRestaurantById(restaurantInput);
+  }
+
+  @Query(() => SearchRestaurantOutput)
+  searchRestaurant(
+    @Args("input") searchRestaurantInput: SearchRestaurantInput,
+  ): Promise<SearchRestaurantOutput> {
+    return this.restaurantService.searchRestaurantByName(searchRestaurantInput);
   }
 }
 
--- src/restaurant/restaurant.service.ts
+++ src/restaurant/restaurant.service.ts
@@ -20,6 +20,11 @@ import { Restaurant } from "./entities/restaurant.entity";
 import { CategoryRepository } from "./repositories/category.repository";
 import { CategoryInput, CategoryOutput } from "./dtos/category.dto";
 import { RestaurantsInput, RestaurantsOutput } from "./dtos/restaurants.dto";
+import { RestaurantInput, RestaurantOutput } from "./dtos/restaurant.dto";
+import {
+  SearchRestaurantInput,
+  SearchRestaurantOutput,
+} from "./dtos/search-restaurant.dto";
 
 @Injectable()
 export class RestaurantService {
@@ -219,6 +224,54 @@ export class RestaurantService {
         ok: false,
         error: "Could not load restaurants",
       };
+    }
+  }
+
+  async findRestaurantById({
+    restaurantId,
+  }: RestaurantInput): Promise<RestaurantOutput> {
+    try {
+      const restaurant = await this.restaurants.findOne({
+        where: { id: restaurantId },
+      });
+      if (!restaurant) {
+        return {
+          ok: false,
+          error: "Restaurant not found",
+        };
+      }
+      return {
+        ok: true,
+        restaurant,
+      };
+    } catch {
+      return {
+        ok: false,
+        error: "Could not find restaurant",
+      };
+    }
+  }
+
+  async searchRestaurantByName({
+    query,
+    page,
+  }: SearchRestaurantInput): Promise<SearchRestaurantOutput> {
+    try {
+      const [restaurants, totalResults] = await this.restaurants.findAndCount({
+        where: {
+          name: Raw((name) => `${name} ILIKE '%${query}%'`),
+        },
+        skip: (page - 1) * 25,
+        take: 25,
+      });
+      return {
+        ok: true,
+        restaurants,
+        totalResults,
+        totalPages: Math.ceil(totalResults / 25),
+      };
+    } catch {
+      return { ok: false, error: "Could not search for restaurants" };
     }
   }
 }
