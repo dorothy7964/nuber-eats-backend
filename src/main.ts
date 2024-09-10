de8dea3d705847a18b57dffdc6e0f10f10711eda
wam <dorothy_007@naver.com>
2024-09-11 02:24:41 +0900

    .env 파일에 도메인 설정

--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,16 @@
 import { ValidationPipe } from "@nestjs/common";
 import { NestFactory } from "@nestjs/core";
 import { AppModule } from "./app.module";
+import { ConfigService } from "@nestjs/config";
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe());
   /* CORS 미들웨어를 사용하여 CORS 설정 적용 */
+  const configService = app.get(ConfigService);
+  const corsOrigin = configService.get<string>("CORS_ORIGIN")?.split(",");
   app.enableCors({
-    origin: ["http://localhost:3000", "https://studio.apollographql.com"], // 허용할 출처
+    origin: corsOrigin, // 허용할 출처
     methods: "GET,POST,PUT,DELETE", // 허용할 HTTP 메서드
     allowedHeaders: "Content-Type, Accept", // 허용할 헤더
   });
