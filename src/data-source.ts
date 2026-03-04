f808c5fa748bfb6e071763ab9fb5c52dafeae229
wam <dorothy_007@naver.com>
2026-03-04 23:43:07 +0900

    배포 환경에서는 dotenv 로드하지 않음

--- src/data-source.ts
+++ src/data-source.ts
@@ -9,11 +9,13 @@ import { Order } from "./order/entities/order.entity";
 import { OrderItem, OrderItemOption } from "./order/entities/order-item.entity";
 import { Payment } from "./payment/entities/payment.entity";
 
-// NODE_ENV에 따라 .env 파일 선택 (배포 환경 제외)
-const envFile = process.env.NODE_ENV === "test" ? ".env.test" : ".env.dev";
-dotenv.config({ path: envFile });
-
-console.log("🚀 개발환경 카테고리 마이그레이션 Loaded ENV file:", envFile);
+// 배포 환경에서는 dotenv 로드하지 않음
+if (process.env.NODE_ENV !== "prod") {
+  // NODE_ENV에 따라 .env 파일 선택 (배포 환경 제외)
+  const envFile = process.env.NODE_ENV === "test" ? ".env.test" : ".env.dev";
+  dotenv.config({ path: envFile });
+  console.log("🚀 개발환경 카테고리 마이그레이션 Loaded ENV file:", envFile);
+}
 
 export const AppDataSource = new DataSource({
   type: "postgres",
@@ -36,4 +38,7 @@ export const AppDataSource = new DataSource({
     Payment,
   ],
   migrations: [__dirname + "/migrations/**/*{.ts,.js}"],
+  ssl: {
+    rejectUnauthorized: false, // Supabase는 ssl 없으면 터진다.
+  },
 });
